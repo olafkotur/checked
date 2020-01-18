dbc36004d69949b00b35343f51640458db0f6a97
olafkotur <olafkotur97@gmail.com>
2020-01-18 14:59:47 +0000

    Changed api misc handlers and add api documentation handler

--- api/src/handlers/misc.ts
+++ api/src/handlers/misc.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import moment from 'moment';
 import { ISimpleResponse } from '../models';
 
-export const PingHandler = {
+export const MiscHandler = {
 
   getPingResponse: (_req: express.Request, res: express.Response) => {
     const response: ISimpleResponse = {
@@ -11,5 +11,9 @@ export const PingHandler = {
     };
 
     res.send(response);
+  },
+
+  getDocumentation: (_req: express.Request, res: express.Response) => {
+    res.redirect('https://documenter.getpostman.com/view/8555555/SWT5gzbe?version=latest');
   }
 }
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { MongoService } from './services/mongo';
-import { PingHandler } from './handlers/ping';
+import { MiscHandler } from './handlers/misc';
 
 const PORT = process.env.PORT || 8080;
 const app: express.Application = express();
@@ -9,7 +9,8 @@ async function main() {
   await MongoService.connect();
 
   // Misc handlers
-  app.get('/api/ping', PingHandler.getPingResponse);
+  app.get('/api/ping', MiscHandler.getPingResponse);
+  app.get('/api/docs', MiscHandler.getDocumentation);
 
   app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
 
