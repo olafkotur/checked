65a30ed5db098bb9d1cdd0193c0f088e1a7549c4
gapark <georgeanthonypark@gmail.com>
2020-01-22 17:10:31 +0000

    Initial dashboard setup

--- checked-web/src/App.tsx
+++ checked-web/src/App.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Router, Link } from "@reach/router";
 
+import MenuBar from './components/MenuBar';
+
 import './index.css';
 
+const Meme = (props: any): JSX.Element =>  {
+	return (
+		<div className="dashContainer">
+			meme
+		</div>
+	);
+};
+
+const Meme2 = (props: any): JSX.Element => {
+	return (
+		<div className="dashContainer">
+			2
+		</div>
+	);
+};
+
+
+
 const App: React.FC = () => {
-  return (
-    <div>
-      memes
-    </div>
-  );
+  	return (
+		<div>
+			<MenuBar />
+      		<Router>
+        		<Meme path="/" />
+				<Meme2 path="meme" />
+      		</Router>
+    	</div>
+  	);
 };
 
 export default App;
---
+++ checked-web/src/components/MenuBar.tsx
@@ -0,0 +1,98 @@
+
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Drawer, ListItemIcon, List, Divider, ListItem, ListItemText } from '@material-ui/core/';
+import { Info as Icon, Menu } from '@material-ui/icons/';
+import { navigate } from '@reach/router';
+
+import Logo from '../media/checkedLogo.jpg';
+
+interface IState {
+    drawerOpen: boolean;
+    drawerClass: string;
+    dimmerClass: string;
+    open: boolean;
+}
+
+interface IProps {
+}
+
+class MenuBar extends React.Component<IProps, IState> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = { drawerOpen: false, drawerClass: 'drawer drawerClosed', dimmerClass: 'dimmer fadeOut', open: false };
+    }
+
+    toggleDrawer = (): void => {
+        if (this.state.drawerOpen === true) {
+            this.setState({ drawerOpen: false, drawerClass: 'drawer drawerClosed', dimmerClass: 'dimmer fadeOut', open: false });
+        } else {
+            this.setState({ drawerOpen: true, drawerClass: 'drawer drawerOpen', dimmerClass: 'dimmer fadeIn' });
+        }
+    };
+
+    setOpen = (): void => {
+        this.setState({ open: true });
+    };
+
+    render(): JSX.Element {
+
+        return (
+            <div>
+                <AppBar position="fixed" className="topBar" style={{ zIndex: 9999 }}>
+                    <Toolbar>
+                        <IconButton onClick={this.toggleDrawer} >
+                            <Menu className="menuIcon" />
+                        </IconButton>
+                        <div className="text-center w-100">
+                            <Typography variant="h6" className="pl-3">
+                                Checked
+                            </Typography>
+                        </div>
+
+                        <div aria-label="logo">
+                            <img src={Logo} className="topBarLogo" alt="logo" />
+                        </div>
+                    </Toolbar>
+                </AppBar>
+
+                <div>
+                    <Drawer
+                        open={this.state.drawerOpen}
+                        variant="permanent">
+                        <div
+                            role="presentation"
+                            className={this.state.drawerClass}
+                        >
+                            <List>
+                                <ListItem button onClick={(): any => navigate('/')} key={'placeholder'} >
+                                    <ListItemIcon><Icon /></ListItemIcon>
+                                    <ListItemText>Memes</ListItemText>
+                                </ListItem>
+                                <ListItem button onClick={(): any => navigate('meme')} key={'placeholder'} >
+                                    <ListItemIcon><Icon /></ListItemIcon>
+                                    <ListItemText>Memes1</ListItemText>
+                                </ListItem>
+
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem button key={'placeholder'} disabled={true}>
+                                    <ListItemIcon><Icon /></ListItemIcon>
+                                    <ListItemText primary={'Placeholder'} />
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Drawer>
+                </div>
+
+                <div className={this.state.dimmerClass} onClick={this.toggleDrawer}>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MenuBar;
