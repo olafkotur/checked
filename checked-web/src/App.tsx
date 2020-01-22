6c9bcc45e836668552c5e0c7bad404f9d8e5ddfa
gapark <georgeanthonypark@gmail.com>
2020-01-22 16:02:05 +0000

    init TS React app for checked-web

---
+++ checked-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router, Link } from "@reach/router";
+
+import './index.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      memes
+    </div>
+  );
+};
+
+export default App;
