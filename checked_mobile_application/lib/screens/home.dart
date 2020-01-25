0b1251d697e3617fc1cc3831c3718e4cfc7c4285
Siddharth Notani <s.notani@lancaster.ac.uk>
2020-01-25 19:43:52 +0000

    Add gridview list

--- checked_mobile_application/lib/screens/home.dart
+++ checked_mobile_application/lib/screens/home.dart
@@ -6,10 +6,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // This is going to be data retrieved from the api - TODP
+  final List<String> _listViewData = [
+    "A List View with many Text - Here's one!",
+    "A List View with many Text - Here's another!",
+    "A List View with many Text - Here's more!",
+    "A List View with many Text - Here's more!",
+    "A List View with many Text - Here's more more!",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Image.asset('assets/checkedLogo.jpg'),
         title: Text('Checked'),
         actions: <Widget>[
           IconButton(
@@ -23,15 +33,22 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Add grid here
-          ],
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(8.0),
+        mainAxisSpacing: 5.0,
+        children: _listViewData
+          .map((data) => Card(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(data),
+              ),
+            ),
+          ))
+          .toList(),
         ),
-      ),
-      floatingActionButton:  FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         //onPressed: ,
         tooltip: 'Add a new zone',
         child: Icon(Icons.add),
