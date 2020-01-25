739b8e899366458752e8d776603a31a796fda8fe
Matthew Russell <russell8@lancaster.ac.uk>
2020-01-25 16:17:03 +0000

    added basic colision detection

--- checked-web/src/components/MapEditor/SaveContainer.tsx
+++ checked-web/src/components/MapEditor/SaveContainer.tsx
@@ -4,49 +4,59 @@ import React from 'react';
 import "./CSS/Editor.css"
 
 import { Save } from '@material-ui/icons';
+import checkCollision from './colisionDetection';
 
 
 const SaveContainer = () => {
     return (
         <div className="blockContainer">
             <div onClick={save} className="saveButton">
-                <Save className="bigIcon"/>
+                <Save className="bigIcon" />
             </div>
 
         </div>
     );
 };
 
-function save(){
+function save() {
     console.log("saving now")
 
     const zones = document.getElementsByClassName("zoneBlock")
     let toSend = []
 
-    for (let i = 0; i < zones.length; i++) {
-        const zone = zones[i];
-        
-        const rect = zone.getBoundingClientRect();
-        const backgroundStyle = window.getComputedStyle(zone, null).getPropertyValue("background-color");
-        // const label = document.getElementById("Zone"+zone.getAttribute('data-name')+"Label");
-        // if (label != null) {
-        //     console.log(label)
-        // }
-        
-        const zoneJson = {
-            id: zone.id,
-            name: zone.getAttribute('data-name'),
-            width: rect.width,
-            height: rect.height,
-            x: rect.x,
-            y: rect.y,
-            color: backgroundStyle,
-        };
-        console.log(zoneJson)
+    if (checkCollision(zones) == false) {
 
+        for (let i = 0; i < zones.length; i++) {
+            const zone = zones[i];
+
+            const rect = zone.getBoundingClientRect();
+            const backgroundStyle = window.getComputedStyle(zone, null).getPropertyValue("background-color");
+            // const label = document.getElementById("Zone"+zone.getAttribute('data-name')+"Label");
+            // if (label != null) {
+            //     console.log(label)
+            // }
+
+            const zoneJson = {
+                id: zone.id,
+                name: zone.getAttribute('data-name'),
+                width: rect.width,
+                height: rect.height,
+                x: rect.x,
+                y: rect.y,
+                color: backgroundStyle,
+            };
+            console.log(zoneJson)
+
+        }
     }
-
+    else {
+        alert("zones cannot intersect")
+    }
     //console.log(zones.length)
 }
+
+
+
+
 
 export default SaveContainer;
