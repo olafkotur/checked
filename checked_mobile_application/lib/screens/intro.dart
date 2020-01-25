51c2eb1a95c07a4fa32912760453be56895a386b
Siddharth Notani <s.notani@lancaster.ac.uk>
2020-01-25 18:48:21 +0000

    Add intro page to show the features

--- checked_mobile_application/lib/screens/intro.dart
+++ checked_mobile_application/lib/screens/intro.dart
@@ -8,11 +8,68 @@ class Intro extends StatefulWidget {
 class _IntroState extends State<Intro> {
   @override
   Widget build(BuildContext context) {
-return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.pink,
-        title: new Text("Intro"),
+    return new Scaffold(
+        body: Container(
+        child: SafeArea(
+          top: true,
+          bottom: false,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height:140.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image(
+                      image: AssetImage("assets/monitor.png"),
+                      height: 200.0
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text("Monitor your kids safely",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.orange[800],
+                ),
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(width: 10),
+                  RaisedButton(
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red)),
+                    onPressed: () {},
+                    color: Colors.orange,
+                    textColor: Colors.white,
+                    child: Text("Get Started!".toUpperCase(),
+                        style: TextStyle(fontSize: 20)),
+                  ),
+                ],   
+              ),
+              SizedBox(
+                height: 290.0,
+              ),
+              Text("Already have an account? Log in here",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.black87,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
-  }
+  } 
 }
