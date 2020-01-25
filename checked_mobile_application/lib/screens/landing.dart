9cde81823149159777e3df3581a2ec61de0725e9
Siddharth Notani <s.notani@lancaster.ac.uk>
2020-01-25 17:43:37 +0000

    Added logo and text to landing page

--- checked_mobile_application/lib/screens/landing.dart
+++ checked_mobile_application/lib/screens/landing.dart
@@ -17,7 +17,46 @@ class _LandingState extends State<Landing> {
             fit: BoxFit.cover,
           ),
         ),
-        child: 
+        child: SafeArea(
+          top: true,
+          bottom: false,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height:220.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image(
+                      image: AssetImage("assets/checkedLogo.jpg"),
+                      height: 200.0
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text("Checked",
+                style: TextStyle(
+                  fontSize: 52.0,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[700],
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text("Keep track of your space",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
