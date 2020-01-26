d3cd7e9ca5026f42fded9cb5c2c41167a8b1613f
Matthew Russell <russell8@lancaster.ac.uk>
2020-01-26 21:06:15 +0000

    added toolbar

--- checked-web/src/pages/map-editor/index.tsx
+++ checked-web/src/pages/map-editor/index.tsx
@@ -4,19 +4,59 @@ import React from 'react';
 import EditorContainer from '../../components/MapEditor/EditorContainer';
 import BackgroundGrid from '../../components/MapEditor/BackgroundGrid';
 import { RouteComponentProps } from '@reach/router';
+import { Card, CardHeader, CardContent, Typography, Avatar, Button, IconButton, Divider } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import SaveContainer from '../../components/MapEditor/SaveContainer';
+import "../../components/MapEditor/CSS/EditorCard.css";
+import { Add, GridOn, Delete, ColorLens, Save } from '@material-ui/icons';
 
 
-
-class MapEditor extends React.Component<RouteComponentProps , {}> {
+class MapEditor extends React.Component<RouteComponentProps, {}> {
 
     render(): JSX.Element {
+
+
+
+
         return (
-            <div>
-                <EditorContainer />
-                <BackgroundGrid />
-            </div>
+            <Card className="editorCard" id="editorCard">
+                <CardHeader title="Room Layout Editor" avatar={<GridOn />}>
+
+                </CardHeader>
+
+                <Divider />
+
+                <Toolbar >
+                    <IconButton aria-label="Add new zone">
+                        <Add />
+                    </IconButton>
+
+                    <IconButton aria-label="Add new zone">
+                        <ColorLens />
+                    </IconButton>
+
+                    <Divider orientation="vertical" variant="middle"  />
+
+                    <IconButton aria-label="Add new zone">
+                        <Delete />
+                    </IconButton>
+
+
+                    <IconButton aria-label="Add new zone">
+                        <Save />
+                    </IconButton>
+                </Toolbar>
+                <Divider />
+                <CardContent > 
+                    
+                </CardContent>
+                
+                {/* <EditorContainer />
+                <BackgroundGrid /> */}
+            </Card>
         );
     }
 };
+
 
 export default MapEditor;
