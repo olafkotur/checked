279a9197dc65eb8f022cc83a9c9df2d85031f3d8
Siddharth Notani <s.notani@lancaster.ac.uk>
2020-01-26 17:57:19 +0000

    Add deleted dragbox

--- checked_mobile_application/lib/screens/home.dart
+++ checked_mobile_application/lib/screens/home.dart
@@ -6,6 +6,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Color deletedColor = Colors.red[200];
+
   // This is going to be data retrieved from the api - TODP
   final List<String> _listViewData = [
     "A List View with many Text - Here's one!",
@@ -19,40 +21,67 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Image.asset('assets/checkedLogo.jpg'),
-        title: Text('Checked'),
+      leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            // do something
+          },
+        ),
+        title: SizedBox(
+          width: 100.0,
+          height: 50.0,
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Image.asset('assets/checkedLogo.jpg'),
+            )
+          ),
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(
-              Icons.menu,
+              Icons.settings,
               color: Colors.black,
             ),
             onPressed: () {
               // do something
             },
-          )
+          ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(8.0),
-        mainAxisSpacing: 5.0,
-        children: _listViewData
-          .map((data) => Card(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(data),
-              ),
-            ),
-          ))
-          .toList(),
-        ),
+      body: Stack(
+        children: <Widget>[
+
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         tooltip: 'Add a new zone',
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class DragBox extends StatefulWidget {
+  final Offset initPos;
+  final String label;
+  final Color itemColor;
+
+  DragBox(this.initPos, this.label, this.itemColor);
+
+  @override
+  _DragBoxState createState() => _DragBoxState();
+}
+
+class _DragBoxState extends State<DragBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
