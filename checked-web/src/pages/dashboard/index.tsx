8e3de5509d4055ae3797ae6df90331f0c3a6ea7b
gapark <georgeanthonypark@gmail.com>
2020-01-27 19:03:48 +0000

    added loader, hooked up backend

--- checked-web/src/pages/dashboard/index.tsx
+++ checked-web/src/pages/dashboard/index.tsx
@@ -7,7 +7,28 @@ import { Card, CardContent, Grid, CardHeader } from '@material-ui/core';
 import { DashboardRounded } from '@material-ui/icons';
 
 import DashTabs from '../../components/DashTabs';
-export class Dashboard extends React.Component<RouteComponentProps, {}> {
+
+interface IState {
+    zones: Array<object>;
+    loaded: boolean;
+}
+export class Dashboard extends React.Component<RouteComponentProps, IState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {zones: [], loaded: false};
+    }
+
+    componentDidMount(): void {
+        ZoneService.loadZones().then((res) => {
+            this.setState({
+                zones: res.result,
+                loaded: true
+            });
+        }).catch(() => {
+            console.error('Error loading Zone Data.');
+        });
+    }
 
     render(): JSX.Element {
 
@@ -16,17 +37,22 @@ export class Dashboard extends React.Component<RouteComponentProps, {}> {
                 <Grid container spacing={3} >
                     <Grid item xs={12} >
                         <Card className="dashCard">
-                            <Grid container spacing={0} >
-                                <Grid item xs={12}>
-                                    <CardHeader title="Dashboard" avatar={<DashboardRounded className="w-100 h-100" />} className="mutedBlack mt-2">
-                                    </CardHeader>
+                            {!this.state.loaded &&
+                                <UseAnimations animationKey="loading2" size={100} className="loginLoader vcenterChild" style={{transform: 'rotate(-90deg)'}}/>
+                            }   
+                            {this.state.loaded && 
+                                <Grid container spacing={0} >
+                                    <Grid item xs={12}>
+                                        <CardHeader title="Dashboard" avatar={<DashboardRounded className="w-100 h-100" />} className="mutedBlack mt-2">
+                                        </CardHeader>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <CardContent className="pt-1 ml-3 mr-3 pl-0 pr-0 border-top border-muted">
+                                            <DashTabs />
+                                        </CardContent>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={12}>
-                                    <CardContent className="pt-1 ml-3 mr-3 pl-0 pr-0 border-top border-muted">
-                                        <DashTabs />
-                                    </CardContent>
-                                </Grid>
-                            </Grid>
+                            }   
                         </Card>
                     </Grid>
                 </Grid>
