f957c6245382b797fbadcb19a6a4ee3120481c99
abdulsalam00 <aboubakar123678@gmail.com>
2020-03-03 17:35:53 +0000

    add base64 convert method / add images to comments

--- checked_mobile_application/lib/screens/newsfeed.dart
+++ checked_mobile_application/lib/screens/newsfeed.dart
@@ -1,7 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:checked_mobile_application/module/api_respose.dart';
 import 'package:checked_mobile_application/screens/notifications.dart';
 import 'package:checked_mobile_application/services/user_services.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:get_it/get_it.dart';
 
 class NewsFeed extends StatefulWidget {
@@ -100,9 +103,12 @@ class _NewsFeedState extends State<NewsFeed> {
                             itemBuilder: (BuildContext context, int index){
                               var timestamp=snapshot.data.data[index]["createdAt"];
                               var date = new DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+                              String rawData = (snapshot.data.data[index]["image"]);
+                              String _base64 = rawData.substring(23, rawData.length);
+                              Uint8List bytes = Base64Decoder().convert(_base64);
                               return Padding(
                                 padding: const EdgeInsets.symmetric(vertical: 5),
-                                child: buildfeedbacktab(context,snapshot.data.data[index]["value"],date),
+                                child: buildfeedbacktab(context,snapshot.data.data[index]["value"],date, bytes),
                               );
                             },
                           );
@@ -119,7 +125,7 @@ class _NewsFeedState extends State<NewsFeed> {
     );
   }
 
-Card buildfeedbacktab(BuildContext context, String comment, DateTime time) {
+Card buildfeedbacktab(BuildContext context, String comment, DateTime time, Uint8List dataimage) {
 return Card(
       elevation: 5,
       child: Padding(
@@ -209,7 +215,7 @@ return Card(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(12.0),
                 image: DecorationImage(
-                  image: AssetImage("assets/checkedLogo.jpg"),
+                  image: MemoryImage(dataimage),
                   fit: BoxFit.cover
                 )
               ),
