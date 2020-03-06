81eb6d7bf3b1dc5583df270492276f46982e6c68
gapark <georgeanthonypark@gmail.com>
2020-03-06 16:13:22 +0000

    fixing settings endpoint

--- checked-web/src/api/SettingsService.ts
+++ checked-web/src/api/SettingsService.ts
@@ -12,8 +12,20 @@ export const SettingsService = {
 
     setUserSettings: async (userId: number, settings: ISettings): Promise<any> => {
         const uri = "settings/update/" + userId;
-        const body = settings;
+
+        const body = {
+            logoImage: settings.logoImage,
+            darkMode: settings.darkMode,
+            timeZone: settings.timeZone,
+            themeColor: settings.themeColor,           
+            interval: settings.notifications.interval,
+            minTemperature: settings.notifications.minTemperature,
+            maxTemperature: settings.notifications.maxTemperature,
+            gatheringThreshold: settings.notifications.gatheringThreshold
+            
+        };
         return await HttpService.post(uri, body).then((res) => {
+            console.log(res);
             return res;
         });
     }
