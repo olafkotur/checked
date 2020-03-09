08c0589c36cc4567564ec3e7737cc90acda56ae3
Matthew Russell <russell8@lancaster.ac.uk>
2020-03-09 00:17:25 +0000

    Revert "added test files"
    This reverts commit e0d641ff13e3418e43a4780ae1b44624880a0cdb.

--- checked-web/src/__tests__/mapEditor.test.tsx
+++ checked-web/src/__tests__/mapEditor.test.tsx
@@ -1,18 +1,9 @@
 
 import MapEditor from "../pages/map-editor";
 
-const editor = new MapEditor({
-    userID: 1
-});
+it('Colision Was Detected Same Values', () => {
 
-it('Set User ID', () => {
-    expect(editor.props.userID).toEqual(1);
-});
-
-it('Text Color White', () => {
-    expect(editor.determineTextColor("#000000")).toEqual("#ffffff");
-});
-
-it('Text Color Black', () => {
-    expect(editor.determineTextColor("#ffffff")).toEqual("#000000");
+    
+   
+    expect(true).toBeTruthy();
 });
